ffd61a9b5d6d5641cfa703b364e2c2c76b74958e
abdulbarik <abdulbariky000@gmail.com>
2024-03-26 14:03:30 +0300

    js functions continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`;
+  }
+
+  function wrapAdjective(adj = 'special') {
+    return function(param) {
+      return `You are ${param} ${adj}!`;
+    };
+  }
+
+ function wrapAdjective(flair = '*') {
+  return function(adj) {
+    if (flair === '||') {
+      return `You are ||${adj}${flair}!`;
+    }
+    return `You are *${adj}${flair}!`;
+  };
+}
